9d17a7a91c7cf23db07e19644504633935d8dc4b
Anselm Brehme <development.anselm@breh.me>
2019-04-27 15:24:21 +0200

    rotate my turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Sprite turretHead;
+    public GameObject head;
+    
+    [Range(0.0f,360.0f)]
+    public float headAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var headrenderer = head.GetComponentInChildren<SpriteRenderer>();
+        headrenderer.sprite = turretHead;
+        head.transform.rotation = Quaternion.Euler(0, 0, headAngle);
+        headAngle += Time.deltaTime * 50;
+    }
+}
