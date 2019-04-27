63355a5469b836653ee856a01a7c91eac30f8a5a
Phillip Schichtel <phillip@schich.tel>
2019-04-28 00:57:36 +0200

    forgot to commit the wall placer

---
+++ Assets/Scripts/WallPlacer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPlacer : Placer
+{
+    public GameObject wallPrefab;
+
+    public WallPlacer() : base(1)
+    {
+    }
+
+    public override GameObject Spawn(Vector2Int gridPos)
+    {
+        var wall = Instantiate(wallPrefab);
+        wall.transform.position = grid.CellToCellCenter(gridPos);
+        return wall;
+    }
+}
