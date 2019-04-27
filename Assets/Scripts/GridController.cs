261298e03960667ba3a5f956d8242659c68fbdec
Phillip Schichtel <phillip@schich.tel>
2019-04-27 16:11:37 +0200

    replace the unity tilemap by a simple grid

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public float cellSize = 1f;
+
+    private float CoordToCell(float coord)
+    {
+        return coord / cellSize;
+    }
+
+    public Vector3 WorldToCellCorner(Vector3 world)
+    {
+        var x = Mathf.Floor(CoordToCell(world.x)) * cellSize;
+        var y = Mathf.Floor(CoordToCell(world.y)) * cellSize;
+        
+        return new Vector3(x, y, world.z);
+    }
+
+    public Vector3 WorldToCellCenter(Vector3 world)
+    {
+        var halfCell = cellSize / 2f;
+        var x = Mathf.Floor(CoordToCell(world.x)) * cellSize + halfCell;
+        var y = Mathf.Floor(CoordToCell(world.y)) * cellSize + halfCell;
+        
+        return new Vector3(x, y, world.z);
+    }
+
+    public Vector2Int WorldToCell(Vector3 world)
+    {
+        return new Vector2Int((int) CoordToCell(world.x), (int) CoordToCell(world.y));
+    }
+}
---
+++ Assets/Scripts/TileHighlighter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class TileHighlighter : MonoBehaviour
+{
+    public GridController grid;
+    public Camera Cam;
+
+    private SpriteRenderer spriteRenderer;
+    private Texture2D texture;
+    private Sprite whiteSprite;
+    
+    void Start()
+    {
+        texture = new Texture2D(1, 1);
+        texture.SetPixel(1, 1, Color.white);
+        whiteSprite = Sprite.Create(texture, new Rect(Vector2.zero, Vector2.one), Vector2.zero);
+
+        transform.position = Cam.ScreenToWorldPoint(Input.mousePosition);
+        spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = whiteSprite;
+        spriteRenderer.drawMode = SpriteDrawMode.Tiled;
+        spriteRenderer.sortingLayerName = "Default";
+        spriteRenderer.size = Vector2.one * grid.cellSize;
+        spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+    }
+
+    void Update()
+    {
+        var worldPos = grid.WorldToCellCorner(Cam.ScreenToWorldPoint(Input.mousePosition));
+        spriteRenderer.size = Vector2.one * grid.cellSize;
+        transform.position = new Vector3(worldPos.x, worldPos.y, 0);
+    }
+}
