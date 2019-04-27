dd1aa5488bd9ac5c22bdd78e9f5dc02b205db45e
Phillip Schichtel <phillip@schich.tel>
2019-04-27 13:04:46 +0200

    added initial rider stuff and scripts

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class TileController : MonoBehaviour
+{
+    private Tilemap tilemap;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.GetAxis("x");
+        Input.GetAxis("y");
+    }
+}
