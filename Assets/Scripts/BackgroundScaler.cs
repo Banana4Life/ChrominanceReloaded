661ce92c65af19bae6adcf989c017cd3cc726e64
Phillip Schichtel <phillip@schich.tel>
2019-04-28 18:37:33 +0200

    scale the background to the viewport size

---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    public Camera cam;
+    private SpriteRenderer r;
+
+    private void Awake()
+    {
+        r = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        var fullSize = cam.orthographicSize * 2f;
+        r.size = new Vector2(fullSize * Screen.width / Screen.height, fullSize);
+    }
+}
