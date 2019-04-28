1fad546924f59e95b90cbd9bd827fdc92aca4974
Phillip Schichtel <phillip@schich.tel>
2019-04-28 21:27:24 +0200

    initial player base

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -6,18 +6,6 @@ public class Base : MonoBehaviour
 {
     public float health = 100;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     void OnTriggerEnter2D(Collider2D other)
     {
         var enemy = other.gameObject.GetComponent<Enemy>();
---
+++ Assets/Scripts/PlayerBaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseController : MonoBehaviour
+{
+    public GridController grid;
+    
+    public int redHealth = 1000;
+    public int greenHealth = 1000;
+    public int blueHealth = 1000;
+
+    public Vector2Int gridLocation = Vector2Int.zero;
+
+    private void Update()
+    {
+        transform.position = grid.CellToCellCenter(gridLocation);
+    }
+
+    public void EnemyReached(Enemy enemy)
+    {
+        Debug.Log("Enemy reached base!");
+    }
+}
