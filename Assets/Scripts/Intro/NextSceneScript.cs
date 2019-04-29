40ddfe1a3d37a62687b4934deabd8908a945246f
Phillip Schichtel <phillip@schich.tel>
2019-04-30 01:33:10 +0200

    intro and stuff

---
+++ Assets/Scripts/Intro/NextSceneScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class NextSceneScript : MonoBehaviour
+{
+    private GameObject loadingText;
+
+    private void Awake()
+    {
+        loadingText = GameObject.Find("Loading");
+        loadingText.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            loadingText.SetActive(true);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
