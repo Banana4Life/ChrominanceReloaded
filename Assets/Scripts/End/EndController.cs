c497bf2c34ecf3493eb30b9c4451ef9f6f4ea52a
Phillip Schichtel <phillip@schich.tel>
2019-04-30 03:13:32 +0200

    end screen

---
+++ Assets/Scripts/End/EndController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace End
+{
+    public class EndController : MonoBehaviour
+    {
+        public void OnExitClick()
+        {
+            if (!Application.isEditor)
+            {
+                Application.Quit();                
+            }
+        }
+
+        public void OnAgainClick()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
